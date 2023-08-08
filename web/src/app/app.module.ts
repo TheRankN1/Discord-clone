e77bb6670c0e6b7a709ff9dd9da45a71506d488b
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2023-08-08 13:07:11 +0300

    resolve comments

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppRoutes } from './app.routes';
 import { ModalCategoryComponent } from './modals/modal-category/modal-category.component';
 import { IsNotEmptyPipe } from './pipes/is-not-empty.pipe';
 import { ModalChannel } from './modals/modal-channel/modal-channel';
-import {SubstringPipe} from "./pipes/substring.pipe";
+import {TextWithEllipsisPipe} from "./pipes/text-with-ellipsis.pipe";
 
 const COMPONENTS = [
   SideBarComponent,
@@ -24,7 +24,7 @@ const COMPONENTS = [
   ModalChannel
 ];
 const MODULES = [CommonModule, BrowserModule, FormsModule, ReactiveFormsModule, AppRoutes];
-const PIPES = [IsNotEmptyPipe , SubstringPipe];
+const PIPES = [IsNotEmptyPipe , TextWithEllipsisPipe];
 @NgModule({
   declarations: [COMPONENTS, PIPES],
   imports: [MODULES],
---
+++ web/src/app/pipes/text-with-ellipsis.pipe.ts
@@ -0,0 +1,12 @@
+import {Pipe, PipeTransform} from '@angular/core';
+
+@Pipe({name: 'TextWithEllipsis'})
+export class TextWithEllipsisPipe implements PipeTransform {
+  transform(name: string, limit:number = 22): string {
+    if(!name){
+      return '';
+    }
+    return (name.length > limit) ? (name.substring(0, limit) + '...') : name;
+  }
+}
+
