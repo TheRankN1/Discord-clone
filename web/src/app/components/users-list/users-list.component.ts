7c71304ef428e02c03004c7baa019623432d9978
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2023-08-15 16:05:19 +0300

    run pretiter

--- web/src/app/components/users-list/users-list.component.ts
+++ web/src/app/components/users-list/users-list.component.ts
@@ -1,10 +1,8 @@
-import {Component} from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector:'app-users-list',
-  templateUrl:'users-list.component.html',
-  styleUrls:['users-list.component.scss']
+  selector: 'app-users-list',
+  templateUrl: 'users-list.component.html',
+  styleUrls: ['users-list.component.scss']
 })
-export class UsersListComponent{
-
-}
+export class UsersListComponent {}
