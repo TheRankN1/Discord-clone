3e710c06a149b2a43671d3b6d8c9bdeec88febf9
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2023-09-07 10:04:46 +0300

    fixes

--- web/src/app/components/server/components/icon/icon.component.ts
+++ web/src/app/components/server/components/icon/icon.component.ts
@@ -1,33 +1,22 @@
 import { Component, HostBinding, HostListener, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
-import { Subscription } from 'rxjs';
+import { Subject, Subscription, takeUntil } from 'rxjs';
 
 const ICONS_PATH = 'assets/icons/svg';
 const ICON_TYPE = '.svg';
 
 @Component({
   selector: 'app-icon',
-  template: '<div [class.color-grey]="isHovered"' + ' (mouseenter)="mouseenter()" (mouseleave)="mouseover()"></div>',
+  template: '<div class="color-grey"></div>',
   styleUrls: ['icon.component.scss']
 })
 export class IconComponent implements OnInit, OnChanges, OnDestroy {
   @Input() public name = '';
   @HostBinding('innerHTML') public svg!: SafeHtml;
-  private subscription = new Subscription();
-  @HostBinding('class.color-white') public isHovered = false;
   @HostBinding('class.color-grey') public isNotHovered = true;
-  @HostListener('mouseenter')
-  mouseenter() {
-    this.isHovered = true;
-    this.isNotHovered = false;
-  }
 
-  @HostListener('mouseleave')
-  mouseover() {
-    this.isHovered = false;
-    this.isNotHovered = true;
-  }
+  private _destroy$: Subject<void> = new Subject<void>();
 
   constructor(
     private sanitizer: DomSanitizer,
@@ -35,24 +24,25 @@ export class IconComponent implements OnInit, OnChanges, OnDestroy {
   ) {}
 
   public ngOnInit(): void {
-    this.subscription.add(
-      this.http.get(`${ICONS_PATH}/${this.name}${ICON_TYPE}`, { responseType: 'text' }).subscribe(value => {
+    this.http
+      .get(`${ICONS_PATH}/${this.name}${ICON_TYPE}`, { responseType: 'text' })
+      .pipe(takeUntil(this._destroy$))
+      .subscribe(value => {
         this.svg = this.sanitizer.bypassSecurityTrustHtml(value);
-      })
-    );
+      });
   }
 
   public ngOnChanges(changes: SimpleChanges): void {
     if (changes['name'] && !changes['name'].firstChange) {
-      this.subscription.add(
-        this.http
-          .get(`${ICONS_PATH}/${this.name}${ICON_TYPE}`, { responseType: 'text' })
-          .subscribe(value => (this.svg = this.sanitizer.bypassSecurityTrustHtml(value)))
-      );
+      this.http
+        .get(`${ICONS_PATH}/${this.name}${ICON_TYPE}`, { responseType: 'text' })
+        .pipe(takeUntil(this._destroy$))
+        .subscribe(value => (this.svg = this.sanitizer.bypassSecurityTrustHtml(value)));
     }
   }
 
   public ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this._destroy$.next();
+    this._destroy$.complete();
   }
 }
