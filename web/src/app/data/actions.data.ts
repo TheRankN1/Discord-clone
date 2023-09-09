cc1da0973abd67bd58f4b04371a966469eb372fe
Sorin Vestemean <vestemeansorin91@gmail.com>
2023-09-09 17:17:39 +0300

    avatar component updated animations (left line) + refactoring + fixed classes

--- web/src/app/data/actions.data.ts
+++ web/src/app/data/actions.data.ts
@@ -3,19 +3,19 @@ import { SidebarActionInterface } from '../interfaces/sidebar-action.interface';
 export const sidebarActions: { [key: string]: SidebarActionInterface } = {
   search: {
     hoverClass: 'fillBackground',
-    fillHoverClass: 'color-green',
+    fillHoverClass: 'fill-green',
     icon: 'search',
     bgColor: '#36373c'
   },
   addServer: {
     hoverClass: 'fillBackground',
-    fillHoverClass: 'color-green',
+    fillHoverClass: 'fill-green',
     icon: 'plus',
     bgColor: '#36373c'
   },
   logout: {
     hoverClass: 'fillBackground',
-    fillHoverClass: 'color-error',
+    fillHoverClass: 'fill-red',
     icon: 'exit',
     bgColor: '#36373c'
   }
