869654643007f8e9fdef1f5f9b09d2cfd6249e70
Sorin Vestemean <vestemeansorin91@gmail.com>
2023-09-10 16:53:12 +0300

    hotfix: tooltip on navigation remains visible

--- web/src/app/shared/components/tooltip/tooltip.component.ts
+++ web/src/app/shared/components/tooltip/tooltip.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-tooltip',
   template: `{{ text }}`,
   styleUrls: ['./tooltip.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TooltipComponent {
   @Input('tooltip') public text = '';
--- web/src/app/shared/components/tooltip/tooltip.directive.ts
+++ web/src/app/shared/components/tooltip/tooltip.directive.ts
@@ -1,8 +1,10 @@
-import { ComponentRef, Directive, ElementRef, HostListener, Input, OnInit } from '@angular/core';
+import { ComponentRef, Directive, ElementRef, HostListener, Input, OnDestroy, OnInit } from '@angular/core';
 import { Overlay, OverlayPositionBuilder, OverlayRef } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { ConnectedPosition } from '@angular/cdk/overlay/position/flexible-connected-position-strategy';
 import { TooltipComponent } from './tooltip.component';
+import { NavigationStart, Router } from '@angular/router';
+import { filter, Subject, takeUntil } from 'rxjs';
 
 type Position = 'top' | 'right' | 'bottom' | 'left';
 const offset = 5;
@@ -10,39 +12,63 @@ const offset = 5;
 @Directive({
   selector: '[tooltip]'
 })
-export class TooltipDirective implements OnInit {
+export class TooltipDirective implements OnInit, OnDestroy {
   @Input('tooltip') public text = '';
   @Input() public tooltipPosition: Position = 'top';
   private _overlayRef!: OverlayRef;
+  private _destroy$ = new Subject<void>();
 
   constructor(
     private _overlay: Overlay,
     private _overlayPositionBuilder: OverlayPositionBuilder,
-    private _elementRef: ElementRef
+    private _elementRef: ElementRef,
+    private _router: Router
   ) {}
 
   public ngOnInit(): void {
     this._setTooltipPosition();
+    this._routeListener();
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
   }
 
   @HostListener('mouseenter')
-  public show(): void {
-    console.log({ show: this.text });
+  private _show(): void {
     const tooltipPortal: ComponentPortal<TooltipComponent> = new ComponentPortal(TooltipComponent);
     const tooltipRef: ComponentRef<TooltipComponent> = this._overlayRef.attach(tooltipPortal);
     tooltipRef.instance.text = this.text;
   }
 
   @HostListener('mouseout')
-  public hide(): void {
-    console.log({ hide: this.text });
+  private _hide(): void {
     this._overlayRef.detach();
+  }
+
+  private _routeListener(): void {
+    this._router.events
+      .pipe(
+        filter((e): e is NavigationStart => e instanceof NavigationStart),
+        takeUntil(this._destroy$)
+      )
+      .subscribe({
+        next: () => this._hide()
+      });
   }
 
   private _setTooltipPosition(): void {
     const positions: { [key: string]: ConnectedPosition } = {
       top: { originX: 'center', originY: 'top', overlayX: 'center', overlayY: 'bottom', offsetX: 0, offsetY: offset },
-      right: { originX: 'end', originY: 'center', overlayX: 'start', overlayY: 'center', offsetX: offset * 2, offsetY: 0 },
+      right: {
+        originX: 'end',
+        originY: 'center',
+        overlayX: 'start',
+        overlayY: 'center',
+        offsetX: offset * 2,
+        offsetY: 0
+      },
       bottom: { originX: 'center', originY: 'bottom', overlayX: 'center', overlayY: 'top', offsetX: 0, offsetY: offset },
       left: { originX: 'start', originY: 'center', overlayX: 'end', overlayY: 'center', offsetX: offset * 2, offsetY: 0 }
     };
