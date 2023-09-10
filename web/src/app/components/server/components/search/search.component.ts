41342dc67d542eb3ca8b138c1b51d19b64e94552
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2023-09-10 09:51:28 +0300

    add message for no servers created yet

--- web/src/app/components/server/components/search/search.component.ts
+++ web/src/app/components/server/components/search/search.component.ts
@@ -1,28 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { ServerInterface } from '../../../../interfaces/server.interface';
-import { ServersService } from '../../../../services/servers.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ServerInterface} from '../../../../interfaces/server.interface';
+import {ServersService} from '../../../../services/servers.service';
+import {Subject, takeUntil} from "rxjs";
 
 @Component({
   selector: 'app-search',
   templateUrl: 'search.component.html',
   styleUrls: ['search.component.scss']
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent implements OnInit, OnDestroy {
   public servers!: Array<ServerInterface>;
   public inputServerName: string = '';
   public loggedUserServers!: Array<ServerInterface>;
   public serverFound!: boolean;
+  private _destroy$: Subject<void> = new Subject<void>();
 
-  constructor(private _serversService: ServersService) {}
+  constructor(private _serversService: ServersService) {
+  }
 
   public ngOnInit(): void {
-    this._serversService.servers$.subscribe({
+    this._serversService.servers$.pipe(takeUntil(this._destroy$)).subscribe({
       next: (servers: Array<ServerInterface>) => {
-        this.servers = servers;
+        this.servers = [...servers];
       }
     });
 
-    this._serversService.loggedUserServers$.subscribe({
+    this._serversService.loggedUserServers$.pipe(takeUntil(this._destroy$)).subscribe({
       next: (servers: Array<ServerInterface>) => {
         this.loggedUserServers = [...servers];
       }
@@ -49,5 +52,10 @@ export class SearchComponent implements OnInit {
   public onSearchInputClear(): void {
     this.inputServerName = '';
     this.serverFound = true;
+  }
+
+  public ngOnDestroy(): void {
+    this._destroy$.next();
+    this._destroy$.complete();
   }
 }
