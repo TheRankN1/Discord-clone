9a25b3a1d17cedae0a2d2d2c749f9ecd26b25078
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2023-09-20 20:03:46 +0300

    add pagination

--- web/src/app/modules/server/views/search/search.component.ts
+++ web/src/app/modules/server/views/search/search.component.ts
@@ -11,7 +11,10 @@ export class SearchComponent implements OnInit {
   public servers!: Array<ServerInterface>;
   public inputServerName: string = '';
   public loggedUserServers!: Array<ServerInterface>;
-  public serverFound!: boolean;
+  public serverFound: boolean = false;
+  public itemsPerPage:number = 2;
+  public currentPage:number = 1;
+  public totalPages:Array<number> = [];
 
   constructor(private _serversService: ServersService) {}
 
@@ -27,10 +30,45 @@ export class SearchComponent implements OnInit {
         this.loggedUserServers = [...servers];
       }
     });
+    this.initializePages();
   }
 
   public joinServer(server: ServerInterface): void {
     this._serversService.joinServer(server);
+  }
+
+  public getTotalPages(): number{
+    return Math.ceil(this.servers.length / this.itemsPerPage)
+  }
+
+  public getCurrentPageItems() {
+    const startIndex = (this.currentPage - 1) * this.itemsPerPage;
+    const endIndex = startIndex + this.itemsPerPage;
+    return this.servers.slice(startIndex, endIndex);
+  }
+
+  public increaseCurrentPage(): void{
+    if(this.currentPage===this.totalPages.length)
+      return;
+    this.currentPage++;
+  }
+
+  public setCurrentPage(page:number):void{
+    this.currentPage = page;
+  }
+
+  public decreaseCurrentPage(): void{
+    if(this.currentPage===1)
+      return;
+    this.currentPage--;
+  }
+
+  public initializePages(): void{
+    const totalPages = this.getTotalPages();
+
+    for(let i=1; i<=totalPages ; i++){
+      this.totalPages.push(i);
+    }
   }
 
   public onSearchInputChanged(): void {
