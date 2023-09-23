8453d3944ab0bac29903812b59ebf8c2f8943a38
Poenaru Petrut <96427905+TheRankN1@users.noreply.github.com>
2023-09-23 22:55:29 +0300

    small updates , implement add roles and delete roles , next step : toggle roles

--- web/src/app/modules/server/components/users-list/users-list.component.ts
+++ web/src/app/modules/server/components/users-list/users-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { UserCategoryInterface } from '../../../../shared/interfaces/user-category.interface';
-import { UserDataBaseInterface } from '../../../../shared/interfaces/user-data-base.interface';
-import { AuthService } from '../../../../shared/services/auth.service';
-import { interval, startWith, Subject, takeUntil } from 'rxjs';
-import { ServersService } from '../../../../shared/services/servers.service';
-import { ServerInterface } from '../../../../shared/interfaces/server.interface';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {UserCategoryInterface} from '../../../../shared/interfaces/user-category.interface';
+import {UserDataBaseInterface} from '../../../../shared/interfaces/user-data-base.interface';
+import {AuthService} from '../../../../shared/services/auth.service';
+import {interval, startWith, Subject, takeUntil} from 'rxjs';
+import {ServersService} from '../../../../shared/services/servers.service';
+import {ServerInterface} from '../../../../shared/interfaces/server.interface';
+import {RoleInterface} from "../../../../shared/interfaces/role.interface";
 
 const INTERVAL_CHECK_ONLINE_STATUS = 5000;
 
@@ -17,27 +18,31 @@ export class UsersListComponent implements OnInit, OnDestroy {
   public categoriesAndUsers: Array<UserCategoryInterface> = [];
   public loggedUser: UserDataBaseInterface | null = null;
   public currentServer!: ServerInterface;
+  public users!: Array<UserDataBaseInterface>;
   private _destroy$ = new Subject<void>();
 
   constructor(
     private _authService: AuthService,
     private _serversService: ServersService
-  ) {}
+  ) {
+  }
 
   public ngOnInit(): void {
     this._initLoggedUserListener();
-    const users: Array<UserDataBaseInterface> = this._authService.users$.value;
+    this._authService.users$.subscribe(users => {
+      this.users = [...users];
+    })
 
     interval(INTERVAL_CHECK_ONLINE_STATUS)
       .pipe(startWith(0), takeUntil(this._destroy$))
       .subscribe({
         next: () => {
-          this._updateUsersStatuses(users);
+          this._updateUsersStatuses(this.users);
         }
       });
     this._serversService.currentServer$.pipe(takeUntil(this._destroy$)).subscribe({
       next: server => {
-        this.currentServer = server;
+        this.currentServer = {...server};
       }
     });
   }
@@ -69,6 +74,66 @@ export class UsersListComponent implements OnInit, OnDestroy {
       }
     });
   }
+
+  public addRole(role: RoleInterface, dBuser: UserDataBaseInterface): void {
+    const newUsersWithRoles: Array<UserDataBaseInterface> = [];
+    if (dBuser.roles.length < this.currentServer.roles.length) {
+      dBuser.roles.push(role);
+    }
+    this.users.forEach((user: UserDataBaseInterface) => {
+      if (user.id === dBuser.id) {
+        newUsersWithRoles.push(dBuser);
+      }
+      this._authService.users$.next(newUsersWithRoles);
+    })
+  }
+
+  public deleteRole(role: RoleInterface, dbUser: UserDataBaseInterface): void {
+    const users = this._authService.users$.value;
+    users.forEach(user => {
+      if (dbUser.id === user.id) {
+        dbUser = user;
+      }
+    })
+    dbUser.roles.forEach(dBuserRole => {
+      const roleIndex = dbUser.roles.indexOf(dBuserRole);
+
+      if (dBuserRole.id === role.id) {
+        dbUser.roles.splice(roleIndex, 1);
+      }
+    })
+    users.forEach(user => {
+      if (dbUser.id === user.id) {
+        user.roles = dbUser.roles;
+      }
+    })
+    this._authService.users$.next(users);
+  }
+
+  public toggleRole(role: RoleInterface, dbUser: UserDataBaseInterface): void {
+    const users = this._authService.users$.value;
+    users.forEach(user => {
+      if (dbUser.id === user.id) {
+        dbUser = user;
+      }
+    })
+    if (dbUser.roles.length === 0) {
+      this.addRole(role, dbUser);
+      return;
+    }
+    dbUser.roles.forEach(dBuserRole => {
+      if (dBuserRole.id !== role.id) {
+        this.addRole(role, dbUser)
+        return;
+      }
+
+      if (dBuserRole.id === role.id) {
+        this.deleteRole(role, dbUser)
+        return;
+      }
+    })
+  }
+
 
   private _initLoggedUserListener(): void {
     this._authService.loggedUser$.pipe(takeUntil(this._destroy$)).subscribe({
